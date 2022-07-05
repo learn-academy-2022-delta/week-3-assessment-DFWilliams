28eb83cb03480915135d376bb0f9751689b7d8f3
DFWilliams <williams.dee.f99@gamil.com>
2022-07-05 07:29:50 -0700

    assessment complete

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,7 @@
 // ASSESSMENT 3: Coding Practical Questions with Jest
 
+const { boolean, number, array } = require("yargs")
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -13,44 +15,132 @@
 
 // --------------------1) Create a function that takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence.
 
-// a) Create a test with expect statements for each of the variables provided.
+// a) Create a test with expect statements for each of the variables provided. 
 
-const fibLength1 = 6
-// Expected output: [1, 1, 2, 3, 5, 8]
+describe("nacciArray", () => {
+    it("takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence", () => {
+        const fibLength1 = 6
+        // Expected output: [1, 1, 2, 3, 5, 8]
 
-const fibLength2 = 10
-// Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+        const fibLength2 = 10
+        // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+    expect(nacciArray(fibLength1)).toEqual([1, 1, 2, 3, 5, 8])
+    expect(nacciArray(fibLength2)).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
+    })
+  })
+
+  //ReferenceError: nacciArray is not defined
 
 
+  
 // b) Create the function that makes the test pass.
 
+//Pseudo Code:
+  // Create a function named nacciArray
+  // set param - num
+  // declare a variable named fibArray as an array with 2 numbers [1,1]
+  // use a for loop to iterate through each number 
+  // use .push add numbers to the array that are in Fibonacci's sequence 
+  // return the array with the newly pushed values
 
+
+ const nacciArray = (num) => {
+    const fibArray = [1,1];
+       for (let i=2; i < num; i++){
+       fibArray.push(fibArray[i-2] + fibArray[i-1])
+    } 
+    return fibArray
+} 
+
+
+//Test Suites: 1 passed, 1 total
+
+
+
+
+ 
+ 
 // --------------------2) Create a function that takes in an array and returns a new array of only odd numbers sorted from least to greatest.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
-// Expected output: [-9, 7, 9, 199]
+describe("leasttoGreatest", () => {
+    it("takes in an array and returns a new array of only odd numbers sorted from least to greatest", () => {
+        const fullArr1 = [4, 9, 0, "7", 8, true, "hey", 7, 199, -9, false, "hola"]
+         // expected output = [-9, 7, 9, 199]
+        
+        const fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
+        // expected output: [-823, 7, 23]
+    expect(leasttoGreatest(fullArr1)).toEqual([-9, 7, 9, 199])
+    expect(leasttoGreatest(fullArr2)).toEqual([-823, 7, 23])
+    })
+  })
 
-const fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
-// Expected output: [-823, 7, 23]
+  //    ReferenceError: leasttoGreatest is not defined
 
 
 // b) Create the function that makes the test pass.
+  
+  //Pseudo code:
+    // declare a function called leasttoGreatest
+    // use .filter to get a new array 
+    //  use .sort with two arguments (a,b) to sort the new from least to greatest
+
+
+const leasttoGreatest = (array) => {
+  const newArray = array.filter (value => typeof
+    value === "number" && value % 2 !== 0
+  ); newArray.sort((a,b) => {return a - b})
+    return newArray
+} 
+
+// console.log (leasttoGreatest(fullArr1))
+
+// Test Suites: 1 passed, 1 total
+
+
 
 
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
 
 // a) Create a test with expect statements for each of the variables provided.
 
-const numbersToAdd1 = [2, 4, 45, 9]
-// Excpected output: [2, 6, 51, 60]
+describe("accumulateSum", () => {
+    it("takes in an array and returns an array of the accumulating sum. An empty array should return an empty array", () => {
+        const numbersToAdd1 = [2, 4, 45, 9]
+        // Excpected output: [2, 6, 51, 60]
 
-const numbersToAdd2 = [0, 7, -8, 12]
-// Expected output: [0, 7, -1, 11]
+        const numbersToAdd2 = [0, 7, -8, 12]
+        // Expected output: [0, 7, -1, 11]
 
-const numbersToAdd3 = []
-// Expected output: []
+        const numbersToAdd3 = []
+        // Expected output: []
+    expect(accumulateSum(numbersToAdd1)).toEqual([2, 6, 51, 60])
+    expect(accumulateSum(numbersToAdd2)).toEqual([0, 7, -1, 11])
+    expect(accumulateSum(numbersToAdd3)).toEqual([])
+    })
+  })
+
+//   ReferenceError: accumulateSum is not defined
 
 
 // b) Create the function that makes the test pass.
+
+  //Psuedo code:
+    // declare a function called accumulateSum 
+    // declare a variable called sum and assign it to 0 
+    // use .map to return an array with the same length 
+    // use addition assignment (+=) to add the values with each iteration
+
+
+
+    const accumulateSum = (array) => {
+        let sum = 0;
+       return array.map (value => {
+            return  sum += value
+        })
+
+    }
+// console.log (accumulateSum(numbersToAdd1))
+
+// Test Suites: 1 passed, 1 total
--- interview-questions.md
+++ interview-questions.md
@@ -9,49 +9,57 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is `this` in JavaScript?
 
-  Your answer:
+  Your answer: this in JavaScript is used when dealing with object. You would you 'this' to call on or reference the object inside of the object. For example, if i had an object called farmAnimals that held data of the animals in the farm and the number of each type of animal and i wanted to create a method called totalAnimals that would give me this total number of all of the animals in the farm you need to add the values of each key. To do this, you need to call on each of the keys within the object by using 'this' to refer to the object and then the key name. 
 
-  Researched answer:
+            ex. let farmAnimals = {
+              chickens : 8
+              pigs : 4
+              cows : 10
+              goats: 2
+              totalAnimals: function (){
+                return this.chickens + this.pigs + this.cows + this.goats
+                }
+
+  Researched answer: 'this' in javascript is used to refer to an object. However, which object the 'this' in refering to is based on how it is used. when used in an event, 'this' refers to the HTML element that the event is acting on. An example would be if you created a method to give an output at the click of a button, you would need to pass the method through the button tag. 'this' with dot notation would be used to refer to the button tage that the event is going to act on. When used on an object, 'this' refers to the object. For example if you where to add a method within on object that added the values of every key in the object together 'this' would be used to refer to the object and with dot notation and the key name to refer to the value that the method is adding. 
 
 
 
 2. What is React? Why would you use it?
 
-  Your answer:
+  Your answer: React is what developers use to create user interactive web pages. React uses components that hold data in a coding language called JSX. The components come together to build the webpages. 
 
-  Researched answer:
-
+  Researched answer: React is a Javascript Library, created by Facebook in 2011, used to create user wed applications. React  React is component based. Components are reusable code that act like javascript functions and return HTML that is displayed on the user interface. 
 
 
 3. Which lifecycle method is required in a React class component?
 
-  Your answer:
+  Your answer: The lifecycle method of a React class component is the constructor method and render method
 
-  Researched answer:
+  Researched answer: The lifecycle method that is required in a react class component is the render method that occurs in two of the three lifecycle stages of a class component, the mounting stage and the updating stage. the render method is the only method required as it holds the data that will be displayed in the user interface. This data is held in fragment tags. 
 
 
 
 4. What is JSX? What is one syntax difference between HTML and JSX?
 
-  Your answer:
+  Your answer: JSX is a coding language that is a hybrid of javascript and HTML syntax. One syntax differnce in JSX from html is that tags have to be held on within fragment tags. 
 
-  Researched answer:
+  Researched answer: JSX is a JavaScript extension that allow you to write HTML and Javascript together. One difference in the syntax is that within JSX you have to hold elements within fragment tags, called a parent element. In HTML you can list as many elements as you would like without holding them in a fragment tag. 
 
 
 
 5. What is yarn? What file(s) are modified in a React application when you run yarn?
 
-  Your answer:
+  Your answer: Yarn is a package manager that contains sharable code. The file that are modified in react when yarn is run is App.js. 
 
-  Researched answer:
+  Researched answer: Yarn is a package manager that allows you to share code with others. It allows you to troubleshoot more easily as you are able to use solution found by other developers. 
 
 
 
 6. STRETCH: What is an anonymous function in JavaScript?
 
-  Your answer:
+  Your answer: An anonymous function is a function without a name.
 
-  Researched answer:
+  Researched answer: An anonymous function is a function that does not have a name instead it take on the keyword function. anonymous functions do not have to be held in a seperate file which helps simplify the code.
 
 
 ## Looking Ahead: Terms for Next Week
